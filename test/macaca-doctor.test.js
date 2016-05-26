2d487ff39ab98c19580c6230a2c742c15d75e5cf
xudafeng <xudafeng@126.com>
2016-05-26 17:55:53 +0800

    tweak catch error

--- test/macaca-doctor.test.js
+++ test/macaca-doctor.test.js
@@ -6,4 +6,9 @@ describe('test', function() {
   it('should be ok', function() {
     Doctor.should.be.ok();
   });
+
+  it('should check ok', function *() {
+    var doctor = new Doctor();
+    yield doctor.check();
+  });
 });
